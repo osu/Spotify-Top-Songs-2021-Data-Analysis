5f33e570ef6712701a9aa7657dbab120566318e9
Hasan Khan <113123618+osu@users.noreply.github.com>
2024-09-30 20:01:54 -0600

    Update song_key_piechart.py

--- Python-Scripts/song_key_piechart.py
+++ Python-Scripts/song_key_piechart.py
@@ -7,16 +7,16 @@ conn = sqlite3.connect('../SpotifyDB.db')
 
 # Query to get the count of each song key
 query = '''
-    SELECT key, COUNT(*) AS total
+    SELECT song_key, COUNT(*) AS total
     FROM Tracks
-    GROUP BY key
+    GROUP BY song_key
     ORDER BY total DESC;
 '''
 df = pd.read_sql(query, conn)
 
 # Create a pie chart
 plt.figure(figsize=(8, 8))
-plt.pie(df['total'], labels=df['key'], autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired.colors)
+plt.pie(df['total'], labels=df['song_key'], autopct='%1.1f%%', startangle=140, colors=plt.cm.Paired.colors)
 plt.title('Distribution of Song Keys')
 plt.tight_layout()
 plt.show()
